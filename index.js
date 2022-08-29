1968a2111bb3205dd51374c8501bd6903be20065
Roshika Perera <pereraroshika98@gmail.com>
2022-08-29 23:24:18 +0530

    intial files added

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const server = require('http').createServer(app);
+
+
+const port = 3000;
+
+
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+app.get('/Hotel/admin', (req, res) => {
+    res.render('admin/adminLayout', { layout: false });
+});
+
+
+server.listen(port, () => {
+    console.log('listening on port ' + port);
+});
