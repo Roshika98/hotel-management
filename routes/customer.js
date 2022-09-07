79530ceae29e4fe390d3889163083e415fa7471b
Roshika Perera <pereraroshika98@gmail.com>
2022-09-07 23:29:56 +0530

    login form created

--- routes/customer.js
+++ routes/customer.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 const auth = require('./authentication');
 
+const custLayout = 'customer/layout';
 
 router.use('/auth/google', auth.googleAuth);
 router.use('/auth/local', auth.localAuth);
 
 
+
 router.get('', (req, res) => {
-    res.render('customer/customerLayout', { layout: false });
+    res.render('customer/partials/welcomePage', { layout: custLayout });
 });
 
 router.get('/auth', (req, res) => {
-    res.send(`<div><button><a href='/hotel/customer/auth/google'>Login With Google</a></button></div>
-    <div><form action='/hotel/customer/auth/local' method='POST'><input type='email' name='email'><input type='password' name='password'>
-    <button>submit</button></form></div>`);
+    res.render('customer/partials/login', { layout: custLayout });
 });
 
 router.get('/auth/logout', (req, res) => {
