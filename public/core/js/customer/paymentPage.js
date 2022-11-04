f96953b7e8a755866a212fa3df18b7fae815b389
Roshika Perera <pereraroshika98@gmail.com>
2022-11-04 12:06:06 +0530

    payment confirmation modal implemented

--- public/core/js/customer/paymentPage.js
+++ public/core/js/customer/paymentPage.js
@@ -3,6 +3,14 @@ const listItems = document.getElementsByClassName('items');
 const nextBtns = document.getElementsByClassName('next');
 const prevBtns = document.getElementsByClassName('previous');
 const submit = document.getElementById('submit');
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+btn.style.display = 'none';
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
 
 var index = 0;
@@ -54,6 +62,27 @@ submit.addEventListener('click', async (event) => {
     event.stopPropagation();
     const params = JSON.stringify(getFormData());
     const response = await axios.post('http://localhost:3000/hotel/customer/reservations/rooms', params, { headers: { 'Content-Type': 'application/json', } });
-    window.location = 'http://localhost:3000/hotel/customer/bookings';
+    // window.location = 'http://localhost:3000/hotel/customer/bookings';
+    btn.click();
 });
 
+
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+    window.location = 'http://localhost:3000/hotel/customer/bookings';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        window.location = 'http://localhost:3000/hotel/customer/bookings';
+    }
+}
